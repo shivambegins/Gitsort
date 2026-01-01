166c7015e4e434d51c675193b37eee2b1d5efbf3
Shivam Singh <engg.shivamsingh7080@gmail.com>
2026-01-01 21:49:47 +0530

    Runtime: 0 ms (100.00%), Memory: 11.7 MB (14.65%) - Gitcode

---
+++ 66-plus-one/66. Plus One.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i = n-1;i>=0;i--){
+        if(digits[i] + 1 != 10){
+             digits[i] = digits[i]+1;
+             return digits;
+        }
+         digits[i] = 0;
+            if(i==0){
+            digits.insert(digits.begin(),1);
+            return digits;
+            }
+         }
+         
+         return digits;
+    }
+};
