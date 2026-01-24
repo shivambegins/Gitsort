f811ecb83c1be21a521813d6068bbcc709d9169f
Shivam Singh <engg.shivamsingh7080@gmail.com>
2026-01-25 02:15:50 +0530

    Runtime: 0 ms (100.00%), Memory: 15.7 MB (32.21%) - Gitcode

--- 31-next-permutation/31. Next Permutation.cpp
+++ 31-next-permutation/31. Next Permutation.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // to find 
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size();
+        // to find those pivot index through which i can change vlaue for next permutation 
+        int idx = -1;
+        // we will compare from right side
+        for(int i = n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]) {
+                idx = i;
+                break;
+                }
+        }
+        // agar pivot nhi mil yani ki index mai change nhi ho rha to reverse karna padega
+        if(idx == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+       reverse(nums.begin()+idx+1, nums.end());
+
+       /// swapping kar rha hu pivot index se next ke liye 
+       int j = -1;
+       for(int i = idx+1;i<n;i++){
+        if(nums[i]>nums[idx]){ j = i;
+        break;
+        }
+       }
+       swap(nums[idx] , nums[j]);
+       return;
     }
 };
